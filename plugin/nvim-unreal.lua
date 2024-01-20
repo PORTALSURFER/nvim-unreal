7bc8e73c3b07868ccc01587a87cfff681808a59d
PORTALSURFER <portalsurfer@gmail.com>
2024-01-20 16:49:04 +0100

    added logging function

--- plugin/nvim-unreal.lua
+++ plugin/nvim-unreal.lua
@@ -1,10 +1,21 @@
-
 -- Check if we are running neovim 0.7.0 or above
 if 1 ~= vim.fn.has "nvim-0.7.0" then
   vim.api.nvim_err_writeln "Unreal.nvim requires at least nvim-0.7.0"
   return
 end
 
+local log_file_path = vim.fn.stdpath "data" .. "/nvim-unreal.log"
+
+local function log(msg)
+  local log_file = io.data(log_file_path, "a")
+  if log_file then
+    log_file:write(msg .. "\n")
+    io.close(log_file)
+  else
+    error("Unable to open log file: " .. log_file_path)
+  end
+end
+
 function setup(args)
-    print("setting up plugin")
+  log("Setting up nvim-unreal")
 end
