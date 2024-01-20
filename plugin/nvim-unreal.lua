43b8e20b65d194f2e9db345739c42ad3771fbd00
PORTALSURFER <portalsurfer@gmail.com>
2024-01-20 17:04:30 +0100

    log changes

--- plugin/nvim-unreal.lua
+++ plugin/nvim-unreal.lua
@@ -1,6 +1,6 @@
 -- Check if we are running neovim 0.7.0 or above
 if 1 ~= vim.fn.has "nvim-0.7.0" then
-  vim.api.nvim_err_writeln "Unreal.nvim requires at least nvim-0.7.0"
+  vim.api.nvim_err_writeln "nvim-unreal requires at least nvim-0.7.0"
   return
 end
 
