df6f0bca6d645605e70cdc17afbdf7efbe82403c
PORTALSURFER <portalsurfer@gmail.com>
2024-01-20 18:21:15 +0100

    added date to logger

--- plugin/nvim-unreal.lua
+++ plugin/nvim-unreal.lua
@@ -7,9 +7,12 @@
 local log_file_path = vim.fn.stdpath("data") .. "/nvim-unreal.log"
 
 local function log(message)
+  local timestamp = os.date("%Y-%m-%d %H:%M:%S")
+  local output = string.format("[%s] %s", timestamp, message)
+
   local log_file = io.open(log_file_path, "a")
   if log_file then
-    log_file:write(message .. "\n")
+    log_file:write(output .. "\n")
    io.close(log_file)
   else
     error("Unable to open log file: " .. log_file_path)
@@ -32,6 +35,7 @@ function setup_nvim_unreal()
 --   print("In setup function")
 --    local log_file = io.open(log_file_path, "a")
     -- setup code
+  --
   log("Setting up nvim-unreal")
 end
 
