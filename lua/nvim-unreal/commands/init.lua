b2a0e1a2115ad0df1a98cb5285ecb763ca24d72f
PORTALSURFER <portalsurfer@gmail.com>
2024-01-20 19:18:34 +0100

    try and fix

--- lua/nvim-unreal/commands/init.lua
+++ lua/nvim-unreal/commands/init.lua
@@ -6,12 +6,13 @@ local commands_list = {
   {
     name = "UnrealGenerateCode",
     func = commands.generate,
-    nargs = 0,
-  } }
+    opts = {},
+  }
+}
 
 function Module.register()
   for _, command in ipairs(commands_list) do
-    vim.api.nvim_create_user_command(command.name, command.func, { nargs = command.nargs })
+    vim.api.nvim_create_user_command(command.name, command.func, command.opts )
   end
 end
 
