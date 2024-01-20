0b7c309cb37e2cec878e94e759e581d5e06da389
PORTALSURFER <portalsurfer@gmail.com>
2024-01-20 20:55:07 +0100

    should now search windows reg for install directory

--- lua/nvim-unreal/commands_loader/commands/generate.lua
+++ lua/nvim-unreal/commands_loader/commands/generate.lua
@@ -8,6 +8,23 @@ local Module = {}
 function Module.execute()
   utilities.log("Generating nvim-unreal code")
   noice.notify("UNIMPLEMENTED", "warn")
+
+  local handle, error_message = io.popen("reg query 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Epic Games\\Unreal Engine' /v InstalledDirectory" )
+  if handle then
+    local result = handle:read("*a")
+    handle:close()
+
+    local unreal_path = string.match(result, "InstalledDirectory%s+REG_SZ%s+(.+)")
+
+    if unreal_path then
+      noice.notify("Unreal Engine is installed at " .. unreal_path)
+    else
+      noice.notify("Unable to find Unreal Engine install dir", "error")
+    end
+
+  else 
+    noice.notify("Unable to read windows registry" .. (error_message or "unknown error"), "error")
+  end
 end
 
 return Module
