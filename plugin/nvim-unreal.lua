2b118956bf66560f76cd8bab7d6fb15bd587a32a
PORTALSURFER <portalsurfer@gmail.com>
2024-01-20 16:07:46 +0100

    add setup function

--- plugin/nvim-unreal.lua
+++ plugin/nvim-unreal.lua
@@ -1,4 +1,10 @@
-if 1 ~= vim.fn.has "nvim-0.11.0" then
+
+-- Check if we are running neovim 0.7.0 or above
+if 1 ~= vim.fn.has "nvim-0.7.0" then
   vim.api.nvim_err_writeln "Unreal.nvim requires at least nvim-0.7.0"
   return
+end
+
+function setup(args)
+    print("setting up plugin")
 end
